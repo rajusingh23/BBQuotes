83bc710031faa3764a477a34c1f6674624735910
rajusingh23 <105393928+rajusingh23@users.noreply.github.com>
2025-10-05 07:20:59 +0530

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Raju Singh on 05/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
