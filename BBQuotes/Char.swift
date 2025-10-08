02e55e1a781e016418bb9b97dba9f8a6670677e4
rajusingh23 <105393928+rajusingh23@users.noreply.github.com>
2025-10-08 19:13:07 +0530

    feat: Implement Quote  and Char model for JSON decoding

---
+++ BBQuotes/Char.swift
@@ -0,0 +1,18 @@
+//
+//  Char.swift
+//  BBQuotes
+//
+//  Created by Raju Singh on 08/10/25.
+//
+
+import Foundation
+
+struct Char: Decodable {
+    let name: String
+    let birthday: String
+    let occupations: [String]
+    let images: [URL]
+    let aliases: [String]
+    let status: String
+    let portrayedBy: String
+}
---
+++ BBQuotes/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  BBQuotes
+//
+//  Created by Raju Singh on 08/10/25.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let quote: String
+    let character: String
+}
